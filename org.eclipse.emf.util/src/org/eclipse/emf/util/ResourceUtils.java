572ea5e31fc13a8c36f1cd44e1535a36f989a7e3
thsoft <devnull@localhost>
2010-06-22 18:54:31 +0200

    More EMF resource utilities

--- org.eclipse.emf.util/src/org/eclipse/emf/util/ResourceUtils.java
+++ org.eclipse.emf.util/src/org/eclipse/emf/util/ResourceUtils.java
@@ -10,8 +10,17 @@ public class ResourceUtils {
 	private ResourceUtils() {
 	}
 
+	/**
+	 * Returns the platform resource corresponding to the given EMF resource.
+	 */
 	public static IResource convertEResourceToPlatformResource(Resource eResource) {
-		URI uri = eResource.getURI();
+		return findPlatformResource(eResource.getURI());
+	}
+
+	/**
+	 * Returns the platform resource with the given EMF URI.
+	 */
+	public static IResource findPlatformResource(URI uri) {
 		if (uri.isPlatformResource()) {
 			return ResourcesPlugin.getWorkspace().getRoot().findMember(uri.toPlatformString(true));
 		} else {

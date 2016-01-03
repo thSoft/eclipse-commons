972c50c84f83ebb18059b2fa21f9b6b5c785f27c
Alexander Nittka <nittka@itemis.de>
2016-01-03 08:35:31 +0100

    simplify arrow key scrolling

--- org.eclipse.ui.views.pdf/src/org/eclipse/ui/views/pdf/PdfViewScrollHandler.java
+++ org.eclipse.ui.views.pdf/src/org/eclipse/ui/views/pdf/PdfViewScrollHandler.java
@@ -4,14 +4,14 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.views.file.FileView;
 import org.eclipse.ui.views.file.IFileViewType;
 
 public class PdfViewScrollHandler implements IHandler{
-
-	private static final String SCROLL_DIRECTION_PARAMETER = "org.eclipse.ui.views.pdf.scrollDirection";//$NON-NLS-1$
 
 	private PdfViewPage getPage(){
 		//handler activation ensures active part is FileView
@@ -27,23 +27,22 @@ public class PdfViewScrollHandler implements IHandler{
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		PdfViewPage page = getPage();
-		if(page!=null){
-			String dir=event.getParameter(SCROLL_DIRECTION_PARAMETER);
-			if(!dir.isEmpty()){
-				int hInc=page.getHorizontalBar().getIncrement();
-				int vInc=page.getVerticalBar().getIncrement();
-				Point currentOrigin=page.getOrigin();
-				int xToSet=currentOrigin.x;
-				int yToSet=currentOrigin.y;
-				if(xToSet>=0 && yToSet>=0){
-					switch (dir.charAt(0)){
-					case 'u':yToSet-=vInc;break;
-					case 'd':yToSet+=vInc;break;
-					case 'l':xToSet-=hInc;break;
-					case 'r':xToSet+=hInc;break;
-					}
-					page.setOrigin(xToSet, yToSet);
+		if(page!=null && (event.getTrigger()) instanceof Event){
+			int key=((Event)event.getTrigger()).keyCode;
+			int hInc=page.getHorizontalBar().getIncrement();
+			int vInc=page.getVerticalBar().getIncrement();
+			Point currentOrigin=page.getOrigin();
+			int xToSet=currentOrigin.x;
+			int yToSet=currentOrigin.y;
+			if(xToSet>=0 && yToSet>=0){
+				switch(key){
+					case SWT.ARROW_UP:yToSet-=vInc;break;
+					case SWT.ARROW_DOWN:yToSet+=vInc;break;
+					case SWT.ARROW_LEFT:xToSet-=hInc;break;
+					case SWT.ARROW_RIGHT:xToSet+=hInc;break;
+					default:return null;
 				}
+				page.setOrigin(xToSet, yToSet);
 			}
 		}
 		return null;

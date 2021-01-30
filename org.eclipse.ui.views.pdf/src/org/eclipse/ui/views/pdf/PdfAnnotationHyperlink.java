f2801fd99ab71f6fea1a2aab68a742ece50ff77e
Dénes Harmath <harmathdenes@gmail.com>
2021-01-30 23:53:30 +0100

    #59 Fix some issues with PdfAnnotationHyperlink

--- org.eclipse.ui.views.pdf/src/org/eclipse/ui/views/pdf/PdfAnnotationHyperlink.java
+++ org.eclipse.ui.views.pdf/src/org/eclipse/ui/views/pdf/PdfAnnotationHyperlink.java
@@ -3,38 +3,39 @@ package org.eclipse.ui.views.pdf;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.util.TextEditorUtils;
 
 public class PdfAnnotationHyperlink extends Composite {
 
-	private static int STYLE= getHyperlinkStyle();
-
-	private static int getHyperlinkStyle() {
-		String system = System.getProperty("os.name", "unknown").toLowerCase();
-		if(system.contains("win")) {
-			return SWT.NONE; //prevent potential scrolling problems caused by #32
-		}else {
-			return SWT.TRANSPARENT | SWT.NO_BACKGROUND;
-		}
-	}
-
-	public PdfAnnotationHyperlink(Composite parent, final PdfAnnotation annotation) {
-		super(parent, STYLE);
-		setCursor(new Cursor(Display.getDefault(), SWT.CURSOR_HAND));
-		addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mouseDown(MouseEvent e) {
-				if(annotation.fileURI!=null) {
-					TextEditorUtils.revealPosition(annotation.fileURI, annotation.lineNumber, annotation.columnNumber, 1);
-				}else if(annotation.file!=null) {
-					TextEditorUtils.revealPosition(annotation.file, annotation.lineNumber, annotation.columnNumber, 1);
+	private static MouseListener LISTENER = new MouseAdapter() {
+		@Override
+		public void mouseDown(MouseEvent e) {
+			Object source = e.getSource();
+			if (source instanceof PdfAnnotationHyperlink) {
+				PdfAnnotation annotation = ((PdfAnnotationHyperlink) source).annotation;
+				if (annotation != null) {
+					if (annotation.fileURI != null) {
+						TextEditorUtils.revealPosition(annotation.fileURI, annotation.lineNumber,
+								annotation.columnNumber, 1);
+					} else if (annotation.file != null) {
+						TextEditorUtils.revealPosition(annotation.file, annotation.lineNumber, annotation.columnNumber,
+								1);
+					}
 				}
 			}
-		});
+		}
+	};
+
+	private PdfAnnotation annotation;
+
+	public PdfAnnotationHyperlink(Composite parent, final PdfAnnotation annotation) {
+		super(parent, SWT.TRANSPARENT | SWT.NO_BACKGROUND);
+		this.annotation = annotation;
+		setCursor(Display.getDefault().getSystemCursor(SWT.CURSOR_HAND));
+		addMouseListener(LISTENER);
 	}
 
 }
